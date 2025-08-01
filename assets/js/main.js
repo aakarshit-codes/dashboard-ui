a8180b31df226523fda29cda7d33c07768f5764a
Aakarshit Sharma <mngmnt.mains@gmail.com>
2025-08-01 21:59:26 +0530

    feat: add header layout with avatar and title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 import { setupSidebarToggle } from "./layout/sidebar.js";
+import { setupHeader } from "./layout/header.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     setupSidebarToggle();
+    setupHeader();
 });
